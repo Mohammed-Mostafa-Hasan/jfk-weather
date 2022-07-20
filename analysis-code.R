29da892b21a1e29a5167155a06f05bf11dc6c78c
Mohammed-Mostafa-Hasan <m.mostafa.eng.zu@gmail.com>
2022-07-20 18:31:08 +0200

    initial commit for jfk weather analysis project

---
+++ analysis-code.R
@@ -0,0 +1,15 @@
+#import libray we need for this project 
+library(tidymodels)
+library(tidyverse)
+library(rlang)
+library(tidyr)
+library(magrittr)
+library(ggplot2)
+library(coefplot)
+library(boot)
+library(rsample)
+library(useful)
+library(glmnet)
+library(parallel)
+library(doParallel)
+
